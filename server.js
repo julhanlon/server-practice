cd988412c456f15ac4f097ade470322a181e89d7
Julia Hanlon <jhanlon289@gmail.com>
2020-06-13 18:35:54 -0700

    updates

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const clientRoutes = require("./routes/client-routes");
+app.use("/", clientRoutes);
+
+const apiRoutes = require("./routes/api-routes");
+app.use(apiRoutes);
 
 app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
